b1ee9680f76b2f7e9a1eef5e321e6415ff6c3496
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2022-05-31 10:07:55 -0400

    problem 16

---
+++ euler0016.js
@@ -0,0 +1,14 @@
+/*
+2^^15 = 32768 and the sum of its digits is 3 + 2 + 7 + 6 + 8 = 26.
+
+What is the sum of the digits of the number 2^1000?
+*/
+
+// Project Euler Solution
+function powerDigitSum (base, exponent) {
+  let number = BigInt(Math.pow(base, exponent));
+  let answer = number.toString().split('').reduce((acc, curr) => parseInt(acc) + parseInt(curr));
+  return answer;
+}
+
+console.log(powerDigitSum(2, 1000));
