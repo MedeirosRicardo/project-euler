3d93e7cb5c477f312204a98187f40c468bc1cec3
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2022-06-02 02:33:00 -0400

    problem 17

--- euler0017.js
+++ euler0017.js
@@ -43,3 +43,30 @@ function numberLetterCount(start, end) {
 }
 
 console.log(numberLetterCount(1, 1000));
+
+// HackerRank Solution
+function processData(input) {
+  const Ones = ["", "One", "Two", "Three", "Four", "Five", "Six", "Seven", "Eight", "Nine", "Ten", "Eleven", "Twelve", "Thirteen", "Fourteen", "Fifteen", "Sixteen", "Seventeen", "Eighteen", "Nineteen"],
+      Tens = ["", "", "Twenty", "Thirty", "Forty", "Fifty", "Sixty", "Seventy", "Eighty", "Ninety", "Hundred"],
+      Scale = ["", "Thousand", "Million", "Billion", "Trillion", "Quadrillion", "Quintillion", "Sextillion"];
+
+  const integerToWords = (n = 0) => {
+      if (n == 0) return "Zero";
+      n = ("0".repeat(2 * (n += "").length % 3) + n).match(/.{3}/g);
+      if (n.length > Scale.length) return "Too Large";
+      let out = "";
+      return n.forEach((Triplet, pos) => {
+          if (+Triplet) {
+              out += ' ' + (+Triplet[0] ? Ones[+Triplet[0]] + ' ' + Tens[10] : "") +
+                  ' ' + (+Triplet.substr(1) < 20 ? Ones[+Triplet.substr(1)] :
+                      Tens[+Triplet[1]] + (+Triplet[2] ? " " : "") + Ones[+Triplet[2]]) +
+                  ' ' + Scale[n.length - pos - 1];
+          }
+      }), out.replace(/\s+/g, ' ').trim();
+  };
+
+  let values = input.split('\n');
+  for (let i = 1; i < values.length; i++) {
+      console.log(integerToWords(values[i]));
+  }
+}
