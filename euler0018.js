a4dfbd69e6748b23dea9b72ba1550a8dd83fab71
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2022-06-04 00:46:15 -0400

    problem 18

--- euler0018.js
+++ euler0018.js
@@ -50,7 +50,6 @@ let Triangle = [
 
 function maximumTrianglePathSum(triangle) {
   for (let i = triangle.length - 1; i > 0; i--) {
-    let row = [];
     for (let j = 0; j < triangle[i].length - 1; j++) {
       triangle[i - 1][j] += Math.max(triangle[i][j] || 0, triangle[i][j + 1] || 0);
     }
