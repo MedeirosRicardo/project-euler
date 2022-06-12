fcabc728617a90862704e3ade3f84fb213eef23a
Ricardo Medeiros <medeiros.ricardo@outlook.com>
2022-06-11 21:50:34 -0400

    problem 20

--- euler0020.js
+++ euler0020.js
@@ -21,8 +21,34 @@ const factorial = (n) => {
 }
 
 const factorialDigitSum = (n) => {
-  let digits = factorial(n).toString().split('').map(elem => parseInt(elem)).reduce((a, b) => a + b);
-  return digits;
+  let sum = factorial(n).toString().split('').map(elem => parseInt(elem)).reduce((a, b) => a + b);
+  return sum;
 }
 
 console.log(factorialDigitSum(100));
+
+// HackerRank Solution
+function processData(input) {
+  const factorial = (n) => {
+    let answer = BigInt(1);
+    if (n === 0 || n === 1) {
+      return answer;
+    } else {
+      for (let i = 2; i <= n; i++) {
+        answer = BigInt(answer) * BigInt(i);
+      }
+      return answer;
+    }
+  }
+
+  const factorialDigitSum = (n) => {
+    let sum = factorial(n).toString().split('').map(elem => parseInt(elem)).reduce((a, b) => a + b);
+    return sum;
+  }
+
+  let values = input.split('\n');
+
+  for (let i = 1; i < values.length; i++) {
+    console.log(factorialDigitSum(values[i]));
+  }
+}
